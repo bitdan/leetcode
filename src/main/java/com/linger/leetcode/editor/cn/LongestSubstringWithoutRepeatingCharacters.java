c4c6743f33001d7b474efa9702720312188def4d
bitdan <abiasnip@gmail.com>
2025-07-02 10:31:02 +0800

    feat(leetcode): 添加最长无重复字符子串问题

---
+++ src/main/java/com/linger/leetcode/editor/cn/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,76 @@
+//给定一个字符串 s ，请你找出其中不含有重复字符的 最长 子串 的长度。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: s = "abcabcbb"
+//输出: 3 
+//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: s = "bbbbb"
+//输出: 1
+//解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: s = "pwwkew"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= s.length <= 5 * 10⁴ 
+// s 由英文字母、数字、符号和空格组成 
+// 
+//
+// Related Topics 哈希表 字符串 滑动窗口 👍 10888 👎 0
+
+
+package com.linger.leetcode.editor.cn;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashSet;
+
+@Slf4j
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        Solution solution = new LongestSubstringWithoutRepeatingCharacters().new Solution();
+        log.info("{}", solution.lengthOfLongestSubstring("abcabcbb"));
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            HashSet<Character> characters = new HashSet<>();
+            int right = 0, maxLen = 0;
+            for (int left = 0; left < s.length(); left++) {
+                if (left != 0) {
+                    characters.remove(s.charAt(left - 1));
+                }
+                while (right < s.length() && !characters.contains(s.charAt(right))) {
+                    characters.add(s.charAt(right));
+                    right++;
+                }
+                maxLen = Math.max(maxLen, right - left);
+            }
+            return maxLen;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}

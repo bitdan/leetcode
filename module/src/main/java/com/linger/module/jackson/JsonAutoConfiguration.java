0910eb16869c50f54e36e07f2804b00937dedae9
bitdan <abiasnip@gmail.com>
2025-07-31 09:23:33 +0800

    refactor(module): 重构 Jackson配置

---
+++ module/src/main/java/com/linger/module/jackson/JsonAutoConfiguration.java
@@ -0,0 +1,29 @@
+package com.linger.module.jackson;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @version 1.0
+ * @description JsonAutoConfiguration
+ * @date 2025/7/31 09:14:02
+ */
+@Slf4j
+@AutoConfiguration(before = JacksonAutoConfiguration.class)
+public class JsonAutoConfiguration {
+
+    /**
+     * springboot 在构建ObjectMapper时默认使用这个构建器
+     *
+     * @return
+     */
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer customizer() {
+        return JsonUtils.CUSTOMIZER;
+    }
+
+}
+
--- module/src/main/java/com/linger/module/jackson/JsonUtils.java
+++ module/src/main/java/com/linger/module/jackson/JsonUtils.java
@@ -1,38 +1,97 @@
 package com.linger.module.jackson;
 
+
 import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimeZone;
 
 /**
  * JSON 工具类
  *
  */
 @Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class JsonUtils {
+    private static final String DEFAULT_DATETIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    private static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+    private static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
+
+    public static final Jackson2ObjectMapperBuilderCustomizer CUSTOMIZER = jackson2ObjectMapperBuilderCustomizer();
 
     private static ObjectMapper objectMapper = new ObjectMapper();
 
-    static {
-        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL); // 忽略 null 值
-        objectMapper.registerModules(new JavaTimeModule()); // 解决 LocalDateTime 的序列化
+
+    /**
+     * 构建 Jackson 自定义配置
+     *
+     * @return
+     */
+    public static Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> {
+            // 这部分也可以在 yaml 中配置
+            builder
+                    // 序列化时，对象为 null，是否抛异常
+                    .failOnEmptyBeans(false)
+                    // 反序列化时，json 中包含 pojo 不存在属性时，是否抛异常
+                    .failOnUnknownProperties(false)
+                    // 禁止将 java.util.Date, Calendar 序列化为数字(时间戳)
+                    .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                    // 设置 java.util.Date, Calendar 序列化、反序列化的格式
+                    .dateFormat(new SimpleDateFormat(DEFAULT_DATETIME_PATTERN))
+                    // 设置 java.util.Date、Calendar 序列化、反序列化的时区
+                    .timeZone(TimeZone.getTimeZone("GMT+8"))
+            ;
+            // 配置 Jackson 序列化 BigDecimal 时使用的格式
+            builder.serializerByType(BigDecimal.class, ToStringSerializer.instance);
+
+            // 配置 Jackson 序列化 long类型为String，解决后端返回的Long类型在前端精度丢失的问题
+            builder.serializerByType(BigInteger.class, NumberSerializer.INSTANCE);
+            builder.serializerByType(Long.class, NumberSerializer.INSTANCE);
+            builder.serializerByType(Long.TYPE, NumberSerializer.INSTANCE);
+
+            // 配置 Jackson 序列化 LocalDateTime、LocalDate、LocalTime 时使用的格式
+            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATETIME_PATTERN)));
+            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT)));
+            builder.serializers(new LocalTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_FORMAT)));
+
+
+            // 配置 Jackson 反序列化 LocalDateTime、LocalDate、LocalTime 时使用的格式
+            builder.deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATETIME_PATTERN)));
+            builder.deserializerByType(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT)));
+            builder.deserializerByType(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_FORMAT)));
+            log.info("init jackson config");
+        };
     }
+
 
     /**
      * 初始化 objectMapper 属性
@@ -103,7 +162,7 @@ public class JsonUtils {
      * 使用 {@link #parseObject(String, Class)} 时，在@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS) 的场景下，
      * 如果 text 没有 class 属性，则会报错。此时，使用这个方法，可以解决。
      *
-     * @param text  字符串
+     * @param text 字符串
      * @param clazz 类型
      * @return 对象
      */
@@ -138,7 +197,7 @@ public class JsonUtils {
     /**
      * 解析 JSON 字符串成指定类型的对象，如果解析失败，则返回 null
      *
-     * @param text          字符串
+     * @param text 字符串
      * @param typeReference 类型引用
      * @return 指定类型的对象
      */
@@ -200,11 +259,10 @@ public class JsonUtils {
 
     /**
      * 判断字符串是否为 JSON 类型的字符串
-     *
      * @param str 字符串
      */
     public static boolean isJsonObject(String str) {
         return JSONUtil.isTypeJSONObject(str);
     }
-
 }
+

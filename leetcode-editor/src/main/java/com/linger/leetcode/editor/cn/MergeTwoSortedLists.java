81dd4de4523cce053ef872b3585406ef9425cb6b
bitdan <abiasnip@gmail.com>
2025-09-11 09:52:35 +0800

    feat(leetcode): 添加合并两个有序链表的题目和解决方案
    - 新增 MergeTwoSortedLists 类，包含题目描述和解决方案
    - 添加对应的 markdown 文件，详细描述题目要求和示例
    - 实现了一个简单的链表合并算法，用于将两个有序链表合并成一个新的有序链表

---
+++ leetcode-editor/src/main/java/com/linger/leetcode/editor/cn/MergeTwoSortedLists.java
@@ -0,0 +1,81 @@
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：l1 = [1,2,4], l2 = [1,3,4]
+//输出：[1,1,2,3,4,4]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：l1 = [], l2 = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：l1 = [], l2 = [0]
+//输出：[0]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 两个链表的节点数目范围是 [0, 50] 
+// -100 <= Node.val <= 100 
+// l1 和 l2 均按 非递减顺序 排列 
+// 
+//
+// Related Topics 递归 链表 👍 3851 👎 0
+
+
+package com.linger.leetcode.editor.cn;
+
+import com.linger.leetcode.editor.cn.doc.ListNode;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        Solution solution = new MergeTwoSortedLists().new Solution();
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+        log.info("{}", solution.mergeTwoLists(l1, l2));
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+
+    class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            ListNode listNode = new ListNode(-1);
+            ListNode cur = listNode;
+            while (list1 != null && list2 != null) {
+                if (list1.val < list2.val) {
+                    cur.next = list1;
+                    list1 = list1.next;
+                } else {
+                    cur.next = list2;
+                    list2 = list2.next;
+                }
+                cur = cur.next;
+            }
+            cur.next = list1 != null ? list1 : list2;
+
+
+            return listNode.next;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}

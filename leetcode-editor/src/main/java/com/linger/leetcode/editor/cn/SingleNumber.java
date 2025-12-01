01882470f85b5724714571215637c371bb69ea1a
linger <abiasnip@gmail.com>
2025-12-01 22:26:46 +0800

    feat(leetcode): 添加只出现一次的数字解法
    - 实现了使用异或运算找出唯一单次出现元素的算法
    - 时间复杂度为 O(n)，空间复杂度为 O(1)
    - 添加了完整的题目描述和示例说明
    - 包含了三个测试用例验证算法正确性
    - 使用位运算技巧满足线性时间和常量空间要求

---
+++ leetcode-editor/src/main/java/com/linger/leetcode/editor/cn/SingleNumber.java
@@ -0,0 +1,73 @@
+//给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。 
+//
+// 你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。 
+//
+// 
+// 
+// 
+// 
+// 
+//
+// 示例 1 ： 
+//
+// 
+// 输入：nums = [2,2,1] 
+// 
+//
+// 输出：1 
+//
+// 示例 2 ： 
+//
+// 
+// 输入：nums = [4,1,2,1,2] 
+// 
+//
+// 输出：4 
+//
+// 示例 3 ： 
+//
+// 
+// 输入：nums = [1] 
+// 
+//
+// 输出：1 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 3 * 10⁴ 
+// -3 * 10⁴ <= nums[i] <= 3 * 10⁴ 
+// 除了某个元素只出现一次以外，其余每个元素均出现两次。 
+// 
+//
+// Related Topics 位运算 数组 👍 3402 👎 0
+
+
+package com.linger.leetcode.editor.cn;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+
+@Slf4j
+public class SingleNumber {
+    public static void main(String[] args) {
+        Solution solution = new SingleNumber().new Solution();
+        log.info("{}", solution.singleNumber(new int[]{4,1,2,1,2}));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int singleNumber(int[] nums) {
+            int res = 0;
+            for (int num : nums) {
+                res ^= num;
+            }
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}

a47a9d3eab69b3f664379d198c789d8c486f7b2e
bitdan <abiasnip@gmail.com>
2025-12-12 11:16:51 +0800

    feat(application): 添加应用启动日志信息
    - 引入@Slf4j注解以支持日志记录
    - 获取应用上下文及环境配置信息
    - 添加本地与外部访问地址的日志输出
    - 显示应用启动成功的提示信息
    - 处理InetAddress获取本机IP的UnknownHostException异常

--- module/src/main/java/com/linger/OOOOApplication.java
+++ module/src/main/java/com/linger/OOOOApplication.java
@@ -1,16 +1,31 @@
 package com.linger;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @version 1.0
  * @description
  * @date 2025/7/10 10:14:35
  */
+@Slf4j
 @SpringBootApplication
 public class OOOOApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(OOOOApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(OOOOApplication.class, args);
+        Environment env = applicationContext.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port + "/\n\t" +
+                "External: \thttp://" + ip + ":" + port + "/\n\t"
+        );
     }
 }

c6c7a839870b7d82081292a0d3afd1c9701eb7d2
linger <abiasnip@gmail.com>
2026-03-21 22:46:39 +0800

    fix(algorithm): 修复三数之和算法实现
    - 将结果列表变量名从res改为list
    - 修正外层循环边界条件，从nums.length - 2调整为nums.length
    - 重构三数之和判断逻辑，先判断等于0的情况再处理大于小于情况
    - 添加重复元素跳过逻辑，避免重复的三元组结果
    - 优化双指针移动策略，在找到目标值时同时移动左右指针

--- leetcode-editor/src/main/java/com/linger/leetcode/editor/cn/ThreeSum.java
+++ leetcode-editor/src/main/java/com/linger/leetcode/editor/cn/ThreeSum.java
@@ -69,36 +69,36 @@ public class ThreeSum {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public List<List<Integer>> threeSum(int[] nums) {
+            ArrayList<List<Integer>> list = new ArrayList<>();
             Arrays.sort(nums);
-            List<List<Integer>> res = new ArrayList<>();
-            for (int i = 0; i < nums.length - 2; i++) {
-                //去重
-                if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int i = 0; i < nums.length; i++) {
+                if (i > 0 && nums[i] == nums[i - 1]) {
+                    continue;
+                }
                 int left = i + 1;
                 int right = nums.length - 1;
                 while (left < right) {
                     int sum = nums[i] + nums[left] + nums[right];
-                    if (sum > 0) {
-                        right--;
-                    } else if (sum < 0) {
-                        left++;
-                    } else {
-                        List<Integer> list = new ArrayList<>();
-                        list.add(nums[i]);
-                        list.add(nums[left]);
-                        list.add(nums[right]);
-                        res.add(list);
+                    if (sum == 0) {
+                        list.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
                         left++;
                         right--;
-                        //去重
-                        while (left < right && nums[left] == nums[left - 1]) left++;
-                        while (left < right && nums[right] == nums[right + 1]) right--;
+                    } else if (sum<0) {
+                        left++;
 
+                    } else if (sum>0) {
+                        right--;
                     }
                 }
-            }
 
-            return res;
+            }
+            return list;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
